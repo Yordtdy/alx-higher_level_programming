c148038de348f8abf169f2dfe7863cfa8f815db0
yordtdy <jordantarsh@gmail.com>
2023-11-03 04:58:12 -0700

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+def infinite(args):
+    sum = 0
+    for a in args[1:]:
+        sum += int(a)
+    print(sum)
+
+if __name__ == "__main__":
+    import sys
+    infinite(sys.argv)
