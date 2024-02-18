35b4a7cc7a52fd8d7edc07d6441c366261fdffa9
yordtdy <jordantarsh@gmail.com>
2024-02-18 10:56:15 -0800

    only file 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
-"""
-    A script that adds the State object 'Louisiana' to hbtn_0e_6_usa
-    Username, password, dbname will be passed as arguments to the script.
-"""
-
-
+""" prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa"""
 import sys
-from model_state import Base, State
+
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                           sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+from model_state import Base, State
 
-    Session = sessionmaker(bind=engine)
-
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    # create a session
+    Session = sessionmaker(bind=engine)
     session = Session()
-
-    # create the object and add it
-    new_state = State(name='Louisiana')
-    session.add(new_state)
-    session.commit()
-
-    # print state.id
-    state_add = session.query(State).filter(State.name == 'Louisiana').one()
-    print(state_add.id)
-
+    flag = False
+    for state in session.query(State).filter(State.name == sys.argv[4]):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            flag = True
+    if flag is False:
+        print("Not found")
     session.close()
